97d05f8a17aa2f1e09af7122a810960561fb7f48
naziaj <49521887+naziaj@users.noreply.github.com>
2019-07-23 15:53:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let intelligences = ["smart", "dumb", "sharp", "creative", "capable", "genius", "einstein", "clever", "wise", "inventive", "keen"]
+function Check() {
+    let name = document.querySelector("input").value;
+    let random = Math.floor(Math.random() * intelligences.length);
+    // alert( "Dear " + name + " you are "+ intelligences[random]);
+    console.log("Dear " + name + " you are " + intelligences[random] + "!")
+}
+
+
+
